bd519bc38efc1190566b5833a43b6ba46a629dc2
Nurudina <navdic1@etf.unsa.ba>
2021-01-16 14:24:39 +0100

    - Added initialize method for filling tableView

--- src/ba/unsa/etf/rpr/projekat/controllers/SecondController.java
+++ src/ba/unsa/etf/rpr/projekat/controllers/SecondController.java
@@ -1,8 +1,46 @@
 package ba.unsa.etf.rpr.projekat.controllers;
 
+import ba.unsa.etf.rpr.projekat.Inspection;
+import ba.unsa.etf.rpr.projekat.InspectionDAO;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+
+
+
 
 public class SecondController {
+    private InspectionDAO dao;
+    public TableView<Inspection> tableView;
+    public TableColumn<Inspection,String> vehicleCol;
+    public TableColumn<Inspection, String> ownerCol;
+    public TableColumn <Inspection, String> evaluationCol;
+    //public TableColumn <Inspection, String>userCol;
+    private ObservableList<Inspection> inspections;
+
+    public SecondController() {
+        dao =  InspectionDAO.getInstance();
+        inspections = FXCollections.observableArrayList(dao.getInspections());
+    }
+
+    @FXML
+        public void initialize(){
+            tableView.setItems(inspections);
+            vehicleCol.setCellValueFactory(data-> new SimpleStringProperty(data.getValue().getVehicle().getBrand()));
+            ownerCol.setCellValueFactory(data-> new SimpleStringProperty(data.getValue().getOwner().toString()));
+            evaluationCol.setCellValueFactory(data -> new SimpleStringProperty(data.getValue().getInspectionEvaluation().toString()));
+            //  controllorCol.setCellValueFactory(data-> new SimpleStringProperty(data.getValue().getUser().toString()));
+
+            tableView.getSelectionModel().selectedItemProperty().addListener((obs, oldInspection, newInspection)->{
+                tableView.refresh();
+            });
+    }
+
     public void addNewAction(ActionEvent actionEvent){
     }
     public void archiveAction(ActionEvent actionEvent){}
@@ -10,6 +48,7 @@ public class SecondController {
     public void modifyAction(ActionEvent actionEvent){}
     public void identificationAction(ActionEvent actionEvent){}
     public void inspectionAction(ActionEvent actionEvent){}
-    public void archiveInspectionAction(ActionEvent actionEvent){}
+    public void archivedInspectionsAction(ActionEvent actionEvent){}
+    public void archivedClientsAction(ActionEvent actionEvent){}
 
 }
