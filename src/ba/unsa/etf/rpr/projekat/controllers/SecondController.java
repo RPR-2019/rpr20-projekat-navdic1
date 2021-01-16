7aacf231f7c3883bf35a79a03af0db2178e225c0
Nurudina <navdic1@etf.unsa.ba>
2021-01-16 01:54:51 +0100

    - Added action methods

--- src/ba/unsa/etf/rpr/projekat/controllers/SecondController.java
+++ src/ba/unsa/etf/rpr/projekat/controllers/SecondController.java
@@ -1,4 +1,15 @@
 package ba.unsa.etf.rpr.projekat.controllers;
 
+import javafx.event.ActionEvent;
+
 public class SecondController {
+    public void addNewAction(ActionEvent actionEvent){
+    }
+    public void archiveAction(ActionEvent actionEvent){}
+    public void deleteAction(ActionEvent actionEvent){}
+    public void modifyAction(ActionEvent actionEvent){}
+    public void identificationAction(ActionEvent actionEvent){}
+    public void inspectionAction(ActionEvent actionEvent){}
+    public void archiveInspectionAction(ActionEvent actionEvent){}
+
 }
